0c23c672d04b0c91ad6eab54404741d98e8f4e97
NoveliaYuki4982 <noayu02@gmail.com>
2025-09-28 17:51:30 +0200

    final version

--- webapp/lamb_kb_webapp.py
+++ webapp/lamb_kb_webapp.py
@@ -1809,4 +1809,4 @@ if __name__ == '__main__':
                 logger.warning(f"  Path does not exist: {path}")
     
     # Run the Flask application
-    app.run(host='0.0.0.0', port=8081, debug=True) 
+    app.run(host='0.0.0.0', port=8080, debug=True) 
